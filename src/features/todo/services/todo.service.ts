460356bb6fc152e1e17aa9cec17e90c455182f79
cocodev <ajorgenmarten35@gmail.com>
2025-12-12 19:49:22 -0500

    fix: security fix, verify that the pending item is owned by the user

--- src/features/todo/services/todo.service.ts
+++ src/features/todo/services/todo.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import UserEntity from 'src/common/entities/user.entity';
 import TodoRepository from '../repositories/todo.repository';
 import PendingItemEntity from 'src/common/entities/pending-item.entity';
@@ -7,23 +11,36 @@ import PendingItemEntity from 'src/common/entities/pending-item.entity';
 export default class TodoService {
   constructor(private readonly todoRepository: TodoRepository) {}
 
+  private async ensureIsMyResource(user: UserEntity, resourceId: string) {
+    const resource = await this.todoRepository.findPendingItemById(resourceId);
+
+    if (!resource) throw new NotFoundException('Pending item not found');
+
+    if (resource.userId !== user.id)
+      throw new ForbiddenException(
+        'You are not authorized to access this resource',
+      );
+
+    return resource;
+  }
+
   async create(user: UserEntity, content: string) {
     const pendingItem = PendingItemEntity.new(user, content);
     await this.todoRepository.createPendingItem(pendingItem);
-    return pendingItem;
+    return pendingItem.toObject();
   }
 
-  async delete(id: string) {
-    await this.todoRepository.deletePendingItem(id);
+  async delete(user: UserEntity, id: string) {
+    const pendingItem = await this.ensureIsMyResource(user, id);
+    await this.todoRepository.deletePendingItem(pendingItem.id);
     return { message: 'Pending item deleted successfully' };
   }
 
-  async toggle(id: string, value: boolean) {
-    const pendingItem = await this.todoRepository.findPendingItemById(id);
-    if (!pendingItem) throw new NotFoundException('Pending item not found');
+  async toggle(user: UserEntity, id: string, value: boolean) {
+    const pendingItem = await this.ensureIsMyResource(user, id);
     if (pendingItem.status === value) return pendingItem;
     pendingItem.status = value;
     await this.todoRepository.updatePendingItem(pendingItem);
-    return pendingItem;
+    return pendingItem.toObject();
   }
 }
--- src/features/todo/todo.controller.ts
+++ src/features/todo/todo.controller.ts
@@ -14,7 +14,9 @@ import { type Request } from 'express';
 import JwtAccessGuard from '../auth/guards/jwt-access.guard';
 import { CreateTodoDto } from './dtos/create-todo.dto';
 import ParamIdDto from './dtos/param-id.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Tareas pendientes')
 @UseGuards(JwtAccessGuard)
 @Controller('todo')
 export default class TodoController {
@@ -26,17 +28,17 @@ export default class TodoController {
   }
 
   @Delete('delete/:id')
-  async delete(@Param() params: ParamIdDto) {
-    return this.todoService.delete(params.id);
+  async delete(@Req() req: Request, @Param() params: ParamIdDto) {
+    return this.todoService.delete(req.user as UserEntity, params.id);
   }
 
   @Get('confirm/:id')
-  async confirm(@Param() params: ParamIdDto) {
-    return this.todoService.toggle(params.id, true);
+  async confirm(@Req() req: Request, @Param() params: ParamIdDto) {
+    return this.todoService.toggle(req.user as UserEntity, params.id, true);
   }
 
   @Get('unconfirm/:id')
-  async unconfirm(@Param() params: ParamIdDto) {
-    return this.todoService.toggle(params.id, false);
+  async unconfirm(@Req() req: Request, @Param() params: ParamIdDto) {
+    return this.todoService.toggle(req.user as UserEntity, params.id, false);
   }
 }
